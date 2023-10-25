e2d5fe7c40c077ee6464fbd55e8ad3a1a4122fd3
00Gizem00 <gizem_fbl@windowslive.com>
2023-10-25 20:57:46 +0300

    jsonla bir dönüş

--- Questions/models.py
+++ Questions/models.py
@@ -1,24 +1,35 @@
+from tabnanny import verbose
 from django.db import models
 
 # Question, Score, and Answer models are created here.
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.TextField(max_length=250)
+
+    class Meta:
+        db_table = 'question'
+        verbose_name = 'Question'
+        verbose_name_plural = 'questions'
     
     def __str__(self):
         return self.question_text
 
 class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='options')
     answer_text = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'answer'
+        verbose_name = 'Answer'
+        verbose_name_plural = 'answers'
 
     def __str__(self):
         return self.answer_text
 
 
 class Score(models.Model):
-    score_number = models.IntegerField(default=0)
+    score_number = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return str(self.score_number)
--- Questions/static/js/quiz.js
+++ Questions/static/js/quiz.js
@@ -6,13 +6,23 @@ const maskOptions = {
 };
 const mask = IMask(element, maskOptions);
 
+
 function isCurrentStepValid() {
-  
+  const activeElements = document.querySelectorAll('.formContent .active :required');
+
+  for(const element of activeElements) {
+    if(!element.checkValidity()) {
+      element.reportValidity();
+
+      return false;
+    }
+  }
+
+  return true;
 }
 
 $('.formNext').click(function() {
-
-  $('.formContent .active : required')
+  if(!isCurrentStepValid()) {return}
 
   $(this).parent().removeClass('active').next().addClass('active');
   $('.formSteps .active').removeClass('active').next().addClass('active');
@@ -21,4 +31,18 @@ $('.formNext').click(function() {
 $('.formPrev').click(function() {
   $(this).parent().removeClass('active').prev().addClass('active');
   $('.formSteps .active').removeClass('active').prev().addClass('active');
-})
+});
+
+$('input, select, textarea').change(function() {
+  
+});
+
+function createFormSteps() {
+  $('.formStep').each(function(index, element) {
+    $('.formSteps').append(`<li>${element.dataset.step}</li>`)
+  });
+  
+  $('.formSteps li:first, .formStep:first').addClass('active');
+}
+
+createFormSteps();
